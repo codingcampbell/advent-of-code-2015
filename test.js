5b24e8d55e3fe3c63a05116e26a3b38dc1256c7d
Matt Campbell <matt@mattcampbell.net>
2015-12-11 21:21:59 -0500

    :lipstick:

--- test.js
+++ test.js
@@ -3,30 +3,12 @@ var fs = require('fs');
 var cliDays = process.argv.slice(2).map(n => +n).filter(n => n && !isNaN(n));
 
 var answers = {
-  'day-1': {
-    part1: 74,
-    part2: 1795
-  },
-  'day-2': {
-    part1: 1606483,
-    part2: 3842356
-  },
-  'day-3': {
-    part1: 2592,
-    part2: 2360
-  },
-  'day-4': {
-    part1: 282749,
-    part2: 9962624
-  },
-  'day-5': {
-    part1: 258,
-    part2: 53
-  },
-  'day-6': {
-    part1: 377891,
-    part2: 14110788
-  },
+  'day-1': { part1: 74, part2: 1795 },
+  'day-2': { part1: 1606483, part2: 3842356 },
+  'day-3': { part1: 2592, part2: 2360 },
+  'day-4': { part1: 282749, part2: 9962624 },
+  'day-5': { part1: 258, part2: 53 },
+  'day-6': { part1: 377891, part2: 14110788 },
 };
 
 var assert = (left, right, done) => done(left === right ? null : new Error(right + ' instead of ' + left))
